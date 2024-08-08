11a2640c73d59b3629c78d16640e6a4c6f0868f0
Ahmed <ahmedelamin42@gmail.com>
2024-08-08 16:02:44 +0000

    sql

---
+++ 0x00-MySQL_Advanced/metal_bands.sql
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>N854KPMBTD60PD4W</RequestId><HostId>n1sYMd1cMPjS58822Qik313U3EnlYiyXTOJENOLxKZTAFHGZGC8mz5/ja9SB8kvjROu22u66CNQ=</HostId></Error>
